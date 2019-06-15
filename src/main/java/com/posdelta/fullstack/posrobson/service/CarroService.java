31da981f2b664b6e66c8f1e214553c06e025eb21
Robson <robson.silva.cr@gmail.com>
2019-06-15 15:10:24 -0300

    Cadastro e lista de carros

---
+++ src/main/java/com/posdelta/fullstack/posrobson/service/CarroService.java
@@ -0,0 +1,41 @@
+package com.posdelta.fullstack.posrobson.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.posdelta.fullstack.posrobson.model.Carro;
+import com.posdelta.fullstack.posrobson.repository.CarroRepository;
+
+@Service
+public class CarroService {
+	
+	@Autowired
+	private CarroRepository repository;
+	
+	public Carro incluir(Carro Carro) {
+		Carro.setId(null);
+		return repository.save(Carro);
+	}
+	
+	public Carro alterar(Carro Carro) {
+		pesquisaPorId(Carro.getId());
+		return repository.save(Carro);
+	}
+	
+	public void excluir(Long id) {
+		repository.deleteById(id);
+	}
+	
+	public List<Carro> listar(){
+		return repository.findAll();
+	}
+	
+	public Carro pesquisaPorId(Long id) {
+		return repository.findById(id).orElseThrow(()
+				-> new EmptyResultDataAccessException(0));
+	}
+
+}
